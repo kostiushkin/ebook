2044324c7c6dabee77962a501d735da6a449f1ca
Serhii Kostiukevych <s.kostiukevych@rocksolid.com.ua>
2020-09-02 16:45:00 +0300

    Get Request for licenses from id

--- web/modules/custom/ebook/src/Plugin/rest/resource/EbookResource.php
+++ web/modules/custom/ebook/src/Plugin/rest/resource/EbookResource.php
@@ -11,13 +11,37 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  *   id = "get_request",
  *   label = @Translation("First get request"),
  *   uri_paths = {
- *     "canonical" = "/get-request",
+ *     "canonical" = "/get-request/{id}",
  *   }
  * )
  */
 class EbookResource extends ResourceBase {
+  public function get($id) {
+    $status = 200;
 
-  public function get() {
-    return new JsonResponse();
+    if($id){
+      $license = \Drupal::entityTypeManager()->getStorage('license')->load($id);
+      $response = [
+        'id' => $license->id(),
+        'type' => $license->get('type')->getString(),
+        'uuid' => $license->get('uuid')->getString(),
+        'langcode' => $license->get('langcode')->getString(),
+        'name' => $license->get('name')->getString(),
+        'user_id' => $license->get('user_id')->getString(),
+        'status' => $license->get('status')->getString(),
+        'expires_automatically' => $license->get('expires_automatically')->getString(),
+        'expiry' => $license->get('expiry')->getString(),
+        'licensed_entity' => $license->get('licensed_entity')->getString(),
+        'created' => $license->get('created')->getString(),
+        'default_langcode' => $license->get('default_langcode')->getString(),
+        'field_active' => $license->get('field_active')->getString(),
+        'field_datetime' => $license->get('field_datetime')->getString(),
+        'field_education' => $license->get('field_education')->getString(),
+        'field_standalone' => $license->get('field_standalone')->getString(),
+        'field_teacher_user' => $license->get('field_teacher_user')->getValue(),
+        'field_terms' => $license->get('field_terms')->getString()
+      ];
+    }
+    return new JsonResponse($response, $status);
   }
 }
